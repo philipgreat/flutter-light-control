7bf05db89e55e11eba5aef0d7d51a4d34e5ac4bb
philipgreat <philip_chang@163.com>
2019-09-24 00:11:25 +0800

    commit for removing extra files

---
+++ cleangit.sh
@@ -0,0 +1,8 @@
+git add .
+git commit -m"commit for removing extra files"
+git pull
+git push
+git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch build' --prune-empty --tag-name-filter cat -- --all
+git commit -m"remove  extra class"
+git push origin --force --all
+
