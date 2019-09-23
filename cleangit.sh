58c81437be601b8a0df0a21b20e812966f563281
philipgreat <philip_chang@163.com>
2019-09-24 00:16:04 +0800

    commit for removing extra files

--- cleangit.sh
+++ cleangit.sh
@@ -3,6 +3,7 @@ git commit -m"commit for removing extra files"
 git pull
 git push
 git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch build' --prune-empty --tag-name-filter cat -- --all
+git filter-branch --force --index-filter 'git rm -r —cached --ignore-unmatch ios/Flutter/' --prune-empty --tag-name-filter cat -- --all
 git commit -m"remove  extra class"
 git push origin --force --all
 
